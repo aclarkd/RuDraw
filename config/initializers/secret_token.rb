be07b74746fd61a391df0d5d13fac13ee499ae28
Andrew Clark <aclarkd@gmail.com>
2013-03-24 12:29:44 -0700

    scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Draw::Application.config.secret_token = 'dad4e13ed1ae2af4984b16ecda362441634de52b418a4b0496fb240d7773bca81f4aebe5d3025052bf5cfbe78bf206d3897627b37025926b5e7441e69ddaa2fc'
